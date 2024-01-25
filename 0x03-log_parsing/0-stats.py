00a2322f0b047bdf7a0edb9710b362c3c893afce
FabPaul <paulfopa0@gmail.com>
2024-01-25 16:55:21 +0100

    another approach

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,53 +4,38 @@
 import sys
 
 
-def initialize_stats():
-    """Initialize statistics"""
-    results =  {"200": 0, "301": 0, "400": 0,
-                "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    
-    return results
-
-def process_lines(line, stats, total_size, line_count):
-    """Process a single log line and update statistics"""
-    try:
-        parts = line.split()
-        status_code = parts[-2]
-        file_size = int(parts[-1])
-
-        if status_code in stats:
-            stats[status_code] += 1
-
-        total_size += file_size
-        line_count += 1
-
-        if line_count % 10 == 0:
-            print_stats(stats, total_size)
-
-    except (IndexError, ValueError):
-        pass
-
-    return total_size, line_count
-
-def print_stats(stats, total_size):
-    """ Print statistics"""
-    print(f"File size: {total_size}")
-    for code, count in sorted(stats.items()):
-        if count:
-            print(f"{code}: {count}")
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     total_size, line_count = 0, 0
-    statistics = initialize_stats()
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {code: 0 for code in codes}
+
+    def print_stats(stats: dict, total_size: int) -> None:
+        """Print statistics"""
+        print(f"File size: {total_size}")
+        for code, count in sorted(stats.items()):
+            if count:
+                print(f"{code}: {count}")
 
     try:
         for line in sys.stdin:
-            total_size, line_count = process_lines(line.strip(),
-                                                   statistics, total_size, line_count)
-            
-        print_stats(statistics, total_size)
+            line_count += 1
+            data = line.split()
 
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                total_size += int(data[-1])
+            except (IndexError, ValueError):
+                pass
+
+            if line_count % 10 == 0:
+                print_stats(stats, total_size)
+        print_stats(stats, total_size)
     except KeyboardInterrupt:
-        print_stats(statistics, total_size)
+        print_stats(stats, total_size)
         raise
