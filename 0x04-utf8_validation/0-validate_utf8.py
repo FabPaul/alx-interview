3c625dcd8476f9d0e2784a5ad56958ffacfa3d2c
FabPaul <paulfopa0@gmail.com>
2024-02-01 21:51:58 +0100

    created utf-8 validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""UTF-8"""
+
+
+def validUTF8(data):
+    """UTF-8 Function"""
+    current_bytes_number = 0
+
+    bitmask_1 = 1 << 7
+    bitmask_2 = 1 << 6
+
+    for byte in data:
+        bitmask = 1 << 7
+
+        if current_bytes_number == 0:
+            while bitmask & byte:
+                current_bytes_number += 1
+                bitmask = bitmask >> 1
+
+            if current_bytes_number == 0:
+                continue
+
+            if current_bytes_number == 1 or current_bytes_number > 4:
+                return False
+
+        else:
+            if (byte & bitmask_1 and not (byte & bitmask_2)):
+                return True
+            else:
+                return False
+
+        current_bytes_number -= 1
+
+    if current_bytes_number == 0:
+        return True
+
+    return False
