2c4a730be673504a274202748e12348416d18753
FabPaul <paulfopa0@gmail.com>
2024-02-09 17:45:36 +0100

    another approach, same goal

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -4,70 +4,55 @@
 import sys
 
 
-def is_queen(board, row, col, n):
-    """Checks if it's safe to place queen at a given position on the board"""
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, n)):
-        if board[i][j] == 1:
-            return False
-
-    return True
+def find_all_solutions(row, column):
+    current_solutions = [[]]
+    for queen_row in range(row):
+        current_solutions = place_queen_on_board(queen_row,
+                                                 column, current_solutions)
+    return current_solutions
 
 
-def solve_nqueens_util(board, row, n):
-    """Recursively solve the N queens"""
-    # If all queens are placed, print the positions of the queens
-    if row == n:
-        queens_positions = []
-        for i in range(n):
-            for j in range(n):
-                if board[i][j] == 1:
-                    queens_positions.append([i, j])
-        print(queens_positions)
-        return True
-
-    re = False
-    # Place a queen in each column of the current row
-    for col in range(n):
-        if is_queen(board, row, col, n):
-            board[row][col] = 1
-            re = solve_nqueens_util(board, row + 1, n) or re
-            board[row][col] = 0
-
-    return re
+def place_queen_on_board(queen_row, board_size, previous_solutions):
+    safe_positions = []
+    for solution in previous_solutions:
+        for x in range(board_size):
+            if is_safe_position(queen_row, x, solution):
+                safe_positions.append(solution + [x])
+    return safe_positions
 
 
-def n_queens(n):
-    """Solve the nqueen and print all possible solutions"""
-
-    if n < 4:
-        print("N must be at leaset 4")
-        sys.exit(1)
-
-    board = [[0] * n for _ in range(n)]
-
-    if not solve_nqueens_util(board, 0, n):
-        print("No solution exists")
-    return
+def is_safe_position(queen_row, x, current_solution):
+    if x in current_solution:
+        return False
+    else:
+        return all(abs(current_solution[column] - x) != queen_row - column
+                   for column in range(queen_row))
 
 
-if __name__ == "__main__":
+def initialize_board_size():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    # Check if N is a number
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
+    if sys.argv[1].isdigit():
+        board_size = int(sys.argv[1])
+    else:
         print("N must be a number")
         sys.exit(1)
+    if board_size < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return board_size
 
-    n_queens(N)
+
+def solve_n_queens():
+    board_size = initialize_board_size()
+    solutions = find_all_solutions(board_size, board_size)
+    for solution in solutions:
+        formatted_solution = []
+        for queen_row, queen_column in enumerate(solution):
+            formatted_solution.append([queen_row, queen_column])
+        print(formatted_solution)
+
+
+if __name__ == '__main__':
+    solve_n_queens()
