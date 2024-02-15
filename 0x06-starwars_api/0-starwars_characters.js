9078339ddb715e7ba2b11c34d1316b574e498926
FabPaul <paulfopa0@gmail.com>
2024-02-15 17:01:34 +0100

    another way

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,50 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
 
-function starwarscharacters(movieId) {
-    const url = `https://swapi.dev/api/films/${movieId}/`;
+const fetchFilm = (filmId) => {
+  return new Promise((resolve, reject) => {
+    const url = `https://swapi.dev/api/films/${filmId}/`;
     request(url, (error, response, body) => {
-        if (!error && response.statusCode == 200) {
-            const film = JSON.parse(body);
-            console.log(`Characters in ${film.title}:`);
-            film.characters.forEach(characterUrl => {
-                request(characterUrl, (charError, charResponse, charBody) => {
-                    if (!charError && charResponse.statusCode == 200) {
-                        const character = JSON.parse(charBody);
-                        console.log(`- ${character.name}`);
-                    } else {
-                        console.log('Error fetching starwars character:', charError);
-                    }
-                });
-            });
-        } else {
-            console.log('Error fetching film:', error)
-        }
+      if (error) {
+        reject(error);
+      } else {
+        const film = JSON.parse(body);
+        resolve(film);
+      }
     });
-}
+  });
+};
 
-if (!movieId) {
-    console.log('Please enter a movie ID');
+const fetchCharacter = (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        const character = JSON.parse(body);
+        resolve(character);
+      }
+    });
+  });
+};
+
+const filmID = process.argv[2];
+
+if (!filmID) {
+  console.log("Please provide a movie ID.");
 } else {
-    starwarscharacters(movieId);
+  fetchFilm(filmID)
+    .then((film) => {
+      console.log(`Characters in ${film.title}:`);
+      return Promise.all(film.characters.map(fetchCharacter));
+    })
+    .then((characters) => {
+      characters.forEach((character) => {
+        console.log(`- ${character.name}`);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
